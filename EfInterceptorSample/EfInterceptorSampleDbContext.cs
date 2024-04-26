edd7cb12c80dd73ab1661ea4bec5fb8b8af5a791
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-26 10:52:14 -0700

    add db context

---
+++ EfInterceptorSample/EfInterceptorSampleDbContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EfInterceptorSample;
+
+public sealed class EfInterceptorSampleDbContext : DbContext
+{
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ef-interceptor-db;Username=dev;Password=dev");
+  }
+}
