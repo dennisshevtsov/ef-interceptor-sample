4394f321a0dad6fee1e247ab7e2931330eecae35
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-27 07:12:23 -0700

    add product

---
+++ EfInterceptorSample/ProductEntity.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfInterceptorSample;
+
+public sealed class ProductEntity
+{
+  public required Guid ProductId { get; set; }
+
+  public required string Name { get; set; }
+
+  public string? Description { get; set; }
+
+  public ulong Price { get; set; }
+}
